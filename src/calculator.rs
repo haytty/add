66e70b20059fbd6c4028312acd8834723edae8d2
H.N <hayato.n.fl@gmail.com>
2024-06-17 11:06:33 +0900

    Add test.

--- src/calculator.rs
+++ src/calculator.rs
@@ -15,3 +15,24 @@ impl Calculator {
         self.addend1 + self.addend2
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculator_new() {
+        let calculator = Calculator::new(1.0, 2.0);
+        assert_eq!(calculator.addend1, 1.0);
+        assert_eq!(calculator.addend2, 2.0);
+    }
+
+    #[test]
+    fn test_calc() {
+        let calculator = Calculator::new(1.0, 2.0);
+        assert_eq!(calculator.calc(), 3.0);
+
+        let calculator = Calculator::new(3.0, 3.0);
+        assert_eq!(calculator.calc(), 6.0);
+    }
+}
